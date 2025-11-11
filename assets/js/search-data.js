bd3a99130fec6b02ab6ea258c760b256cd7805c6
tushar-nayak <87072557+tushar-nayak@users.noreply.github.com>
2025-11-11 23:43:21 +0000

    Deploying to gh-pages from @ tushar-nayak/tushar-nayak.github.io@bc7f7e65cf2fd10319b5c9b35afc2ca0d14051a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: ".",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "i haven&#39;t taught any courses (yet!), just a record of work as a teaching assistant!",
